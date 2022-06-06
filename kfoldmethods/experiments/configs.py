dfedeb29820910865fb33846ed78412c7efda744
Thomas <thomasvf@protonmail.com>
2022-06-06 15:16:47 -0300

    Use 100 repetitions

--- kfoldmethods/experiments/configs.py
+++ kfoldmethods/experiments/configs.py
@@ -31,6 +31,6 @@ classifier_hyperparameters_output = "%s/classifier_hyperparameters" % run_data_d
 datasets = pmlb_get_ds_list(task='classification', n_samples=(200, 2000), verbose=False)
 datasets = datasets[::3]
 
-true_estimates_n_splits = 5
+true_estimates_n_splits = 100
 true_estimates_n_jobs = 2
 true_estimates_random_state = 123
