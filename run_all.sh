42fc4cac49953990915d1303fcc67c868f968dfc
Thomas <thomasvf@protonmail.com>
2022-08-28 16:32:28 -0300

    Add run instructions

---
+++ run_all.sh
@@ -0,0 +1,11 @@
+conda env create -f environtment_file.yml 
+python main.py hp-search
+python main.py hp-search -s
+python main.py n-clusters-estimate
+python main.py n-clusters-estimate -a
+python main.py true-estimate
+python main.py true-estimate -s
+python main.py true-estimate -a
+python main.py compare-splitters
+python main.py compare-splitters -s
+python main.py compare-splitters -a
